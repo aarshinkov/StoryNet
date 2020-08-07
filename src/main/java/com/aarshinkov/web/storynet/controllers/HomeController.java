8e898144955d0ec0e5faf0c74d6afead2e7fa7cd
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-08-07 19:43:37 +0300

    Цикли

--- src/main/java/com/aarshinkov/web/storynet/controllers/HomeController.java
+++ src/main/java/com/aarshinkov/web/storynet/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.aarshinkov.web.storynet.controllers;
 
+import java.util.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +16,22 @@ public class HomeController
   @GetMapping(value = "/")
   public String home(Model model)
   {
-    int age = 17;
     
-    model.addAttribute("age", age);
+    List<Integer> numbersList = new ArrayList<>();
+    numbersList.add(2);
+    numbersList.add(5);
+    numbersList.add(1);
+    numbersList.add(7);
+    numbersList.add(9);
+    numbersList.add(15);
+
+    List<String> names = new ArrayList<>();
+    names.add("Atanas");
+    names.add("Stefani");
+    names.add("Georgi");
+    
+    model.addAttribute("list", numbersList);
+    model.addAttribute("names", names);
 
     return "home";
   }
