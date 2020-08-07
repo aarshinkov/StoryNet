0951f1890f1798b85e93ba314a249c60339d5b04
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-08-07 18:54:12 +0300

    MVC View конфигурация

--- src/main/java/com/aarshinkov/web/storynet/StoryNetApplication.java
+++ src/main/java/com/aarshinkov/web/storynet/StoryNetApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoryNetApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StoryNetApplication.class, args);
-	}
-
+public class StoryNetApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(StoryNetApplication.class, args);
+  }
 }
---
+++ src/main/java/com/aarshinkov/web/storynet/config/MvcViewConfig.java
@@ -0,0 +1,60 @@
+package com.aarshinkov.web.storynet.config;
+
+import java.util.*;
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+import org.thymeleaf.spring5.*;
+import org.thymeleaf.spring5.templateresolver.*;
+import org.thymeleaf.spring5.view.*;
+import org.thymeleaf.templateresolver.*;
+
+/**
+ *
+ * @author Atanas Yordanov Arshinkov
+ * @since 1.0.0
+ */
+@Configuration
+public class MvcViewConfig implements WebMvcConfigurer
+{
+  @Bean
+  public SpringResourceTemplateResolver srtr()
+  {
+    SpringResourceTemplateResolver srtr = new SpringResourceTemplateResolver();
+    srtr.setPrefix("/WEB-INF/views/");
+    srtr.setSuffix(".html");
+    srtr.setTemplateMode("HTML");
+    srtr.setCharacterEncoding("UTF-8");
+    srtr.setCacheable(false);
+    srtr.setOrder(1);
+
+    return srtr;
+  }
+
+  @Bean
+  public Set<ITemplateResolver> templateResolvers()
+  {
+    Set<ITemplateResolver> templateResolvers = new HashSet<>();
+    templateResolvers.add(srtr());
+
+    return templateResolvers;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine()
+  {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolvers(templateResolvers());
+
+    return templateEngine;
+  }
+
+  @Bean
+  public ThymeleafViewResolver viewResolver()
+  {
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(templateEngine());
+    viewResolver.setCharacterEncoding("UTF-8");
+
+    return viewResolver;
+  }
+}
