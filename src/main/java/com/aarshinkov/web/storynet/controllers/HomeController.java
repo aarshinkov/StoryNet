672631f80d968ad726d2667dea7d1e4ae1d9b317
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-08-07 19:22:11 +0300

    Използване на динамични променливи

--- src/main/java/com/aarshinkov/web/storynet/controllers/HomeController.java
+++ src/main/java/com/aarshinkov/web/storynet/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.aarshinkov.web.storynet.controllers;
 
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -12,8 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController
 {
   @GetMapping(value = "/")
-  public String home()
+  public String home(Model model)
   {
+    double a = 5.8;
+    double b = 8.2;
+
+    String name = "Georgi Petkov";
+    model.addAttribute("name", name);
+
+    model.addAttribute("result", a * b);
+
     return "home";
   }
 }
