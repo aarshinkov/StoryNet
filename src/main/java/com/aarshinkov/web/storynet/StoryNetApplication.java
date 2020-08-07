fb9356933bd80a9bdf7699affdd50b2b3221b445
aarshinkov <a.arshinkov97@gmail.com>
2020-08-07 18:12:10 +0300

    Added project to repository

---
+++ src/main/java/com/aarshinkov/web/storynet/StoryNetApplication.java
@@ -0,0 +1,13 @@
+package com.aarshinkov.web.storynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aarshinkov/web/storynet/StoryNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarshinkov.web.storynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
