1c4d76bdf18d1859e2d5d8d8c453837749459579
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-08-20 19:18:59 +0300

    Thymeleaf шаблон

--- src/main/java/com/aarshinkov/web/storynet/config/MvcViewConfig.java
+++ src/main/java/com/aarshinkov/web/storynet/config/MvcViewConfig.java
@@ -1,8 +1,10 @@
 package com.aarshinkov.web.storynet.config;
 
 import java.util.*;
+import nz.net.ultraq.thymeleaf.*;
 import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.*;
+import org.thymeleaf.dialect.*;
 import org.thymeleaf.spring5.*;
 import org.thymeleaf.spring5.templateresolver.*;
 import org.thymeleaf.spring5.view.*;
@@ -31,6 +33,22 @@ public class MvcViewConfig implements WebMvcConfigurer
   }
 
   @Bean
+  public LayoutDialect layoutDialect()
+  {
+    LayoutDialect layoutDialect = new LayoutDialect();
+    return layoutDialect;
+  }
+
+  @Bean
+  public Set<IDialect> additionalDialects()
+  {
+    Set<IDialect> additionalDialects = new HashSet<>();
+    additionalDialects.add(layoutDialect());
+    return additionalDialects;
+  }
+
+  @Bean
+
   public Set<ITemplateResolver> templateResolvers()
   {
     Set<ITemplateResolver> templateResolvers = new HashSet<>();
@@ -44,6 +62,7 @@ public class MvcViewConfig implements WebMvcConfigurer
   {
     SpringTemplateEngine templateEngine = new SpringTemplateEngine();
     templateEngine.setTemplateResolvers(templateResolvers());
+    templateEngine.setAdditionalDialects(additionalDialects());
 
     return templateEngine;
   }
