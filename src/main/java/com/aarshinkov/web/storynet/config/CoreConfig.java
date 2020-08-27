a19387ed02ae95751abf93bd67221ae0260a6c99
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-08-27 03:40:27 +0300

    Добавяне на фрагмента за нотификации

--- src/main/java/com/aarshinkov/web/storynet/config/CoreConfig.java
+++ src/main/java/com/aarshinkov/web/storynet/config/CoreConfig.java
@@ -14,6 +14,12 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class CoreConfig implements WebMvcConfigurer
 {
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry)
+  {
+    registry.addResourceHandler("/resources/**").addResourceLocations("WEB-INF/resources/");
+  }
+
   @Bean
   public ReloadableResourceBundleMessageSource messageSource()
   {
