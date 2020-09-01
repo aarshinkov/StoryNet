b360f8af7f52cf4f4f3b4bf00a0a98c5479a6d34
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-09-02 02:38:35 +0300

    Създаване на Spring Boot конфигурацията за сигурност

--- src/main/java/com/aarshinkov/web/storynet/StoryNetApplication.java
+++ src/main/java/com/aarshinkov/web/storynet/StoryNetApplication.java
@@ -2,6 +2,9 @@ package com.aarshinkov.web.storynet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
 
 @SpringBootApplication
 public class StoryNetApplication
@@ -9,5 +12,11 @@ public class StoryNetApplication
   public static void main(String[] args)
   {
     SpringApplication.run(StoryNetApplication.class, args);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder()
+  {
+    return new BCryptPasswordEncoder(12);
   }
 }
---
+++ src/main/java/com/aarshinkov/web/storynet/security/WebSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.aarshinkov.web.storynet.security;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.method.configuration.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.web.access.*;
+import org.springframework.security.web.authentication.*;
+import org.springframework.security.web.authentication.logout.*;
+
+/**
+ *
+ * @author Atanas Yordanov Arshinkov
+ * @since 1.0.0
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+  @Autowired
+  private AuthenticationSuccessHandler authSuccessHandler;
+
+  @Autowired
+  private AccessDeniedHandler accessDeniedHandler;
+
+  @Autowired
+  private LogoutSuccessHandler logoutSuccessHandler;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception
+  {
+    http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/", "/home").permitAll()
+            .antMatchers("/signup").anonymous()
+            .and()
+            .formLogin()
+            .successHandler(authSuccessHandler)
+            .and()
+            .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+            .and()
+            .logout()
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessHandler(logoutSuccessHandler)
+            .and()
+            .httpBasic();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception
+  {
+    String password = passwordEncoder.encode("Test-1234");
+
+    auth.inMemoryAuthentication()
+            .withUser("aarshinkov").password(password).roles("ADMIN", "USER")
+            .and()
+            .withUser("snikolov").password(password).roles("USER");
+  }
+}
