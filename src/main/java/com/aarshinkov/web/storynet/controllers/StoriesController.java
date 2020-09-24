e1d9fd4a71715daa0e2eb2342991af4be3d1e1e9
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-09-24 04:16:16 +0300

    СЪЗДАВАНЕ: Добавяне на подготвящия метод

---
+++ src/main/java/com/aarshinkov/web/storynet/controllers/StoriesController.java
@@ -0,0 +1,29 @@
+package com.aarshinkov.web.storynet.controllers;
+
+import com.aarshinkov.web.storynet.base.*;
+import com.aarshinkov.web.storynet.models.stories.*;
+import org.slf4j.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *
+ * @author Atanas Yordanov Arshinkov
+ * @since 1.0.0
+ */
+@Controller
+public class StoriesController extends Base
+{
+  private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+  @GetMapping(value = "/story/create")
+  public String prepareCreateStory(StoryCreateModel scm, Model model)
+  {
+    model.addAttribute("story", scm);
+
+    model.addAttribute("globalMenu", "stories");
+
+    return "stories/create";
+  }
+}
