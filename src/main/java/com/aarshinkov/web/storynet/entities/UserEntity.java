4b826375e7cf1ae4c5ff2a4cbbbdbcdaaa0eaefd
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-09-26 15:33:06 +0300

    СЪЗДАВАНЕ: Добавяне на линка в навигационното меню

--- src/main/java/com/aarshinkov/web/storynet/entities/UserEntity.java
+++ src/main/java/com/aarshinkov/web/storynet/entities/UserEntity.java
@@ -30,6 +30,7 @@ public class UserEntity implements UserDetails, Serializable
   @Id
   @SequenceGenerator(name = "seq_gen_user", sequenceName = "s_users", allocationSize = 1)
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_gen_user")
+  @Column(name = "user_id")
   private Long userId;
 
   @Column(name = "email")
