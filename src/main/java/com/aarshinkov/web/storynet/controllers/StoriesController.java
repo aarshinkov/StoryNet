04eb792038a1de31d7cee53752ae6be942d720d7
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-10-05 13:40:16 +0300

    СЪЗДАВАНЕ: Валидация на данните

--- src/main/java/com/aarshinkov/web/storynet/controllers/StoriesController.java
+++ src/main/java/com/aarshinkov/web/storynet/controllers/StoriesController.java
@@ -2,10 +2,14 @@ package com.aarshinkov.web.storynet.controllers;
 
 import com.aarshinkov.web.storynet.base.*;
 import com.aarshinkov.web.storynet.models.stories.*;
+import javax.servlet.http.*;
+import javax.validation.*;
 import org.slf4j.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
+import org.springframework.validation.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.*;
 
 /**
  *
@@ -33,5 +37,18 @@ public class StoriesController extends Base
     model.addAttribute("globalMenu", "stories");
 
     return "stories/create";
+  }
+
+  @PostMapping(value = "/story/create")
+  public String createStory(@ModelAttribute("story") @Valid StoryCreateModel scm, BindingResult bindingResult,
+          HttpServletRequest request, Model model, RedirectAttributes redirectAttributes)
+  {
+    if (bindingResult.hasErrors())
+    {
+      model.addAttribute("globalMenu", "stories");
+      return "stories/create";
+    }
+
+    return null;
   }
 }
